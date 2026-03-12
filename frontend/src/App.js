15c3f5943721b392255153b82a97afed634d58e1
Himanshu Pandey <himanshupandey153@gmail.com>
2026-03-12 23:39:06 +0530

    Routing system completed with Navbar navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/login";
+import Register from "./pages/Register";
+import ProductScreen from "./pages/ProductScreen";
+import CartScreen from "./pages/CartScreen";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
+  return (
+    <Router>
 
-return (
+      <Navbar />
 
-<div>
-<Login/>
-</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/product/:id" element={<ProductScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+      </Routes>
 
-);
+      <Footer />
 
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{background: "#222", padding: "15px"}}>
+      <Link to="/" style={{color:"white", marginRight:"20px"}}>Home</Link>
+      <Link to="/login" style={{color:"white", marginRight:"20px"}}>Login</Link>
+      <Link to="/register" style={{color:"white", marginRight:"20px"}}>Register</Link>
+      <Link to="/cart" style={{color:"white"}}>Cart</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
